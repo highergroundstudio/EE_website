e0e4a024174e5824be005696bc4e7a16b625309c
Kyle King <highergroundstudio@users.noreply.github.com>
2019-04-19 10:52:15 -0600

    Moved to gatsby univeral starter

---
+++ site-config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = {
+  siteTitle: `Elevated Equities`,
+  siteTitleShort: `EE`,
+  siteDescription: `Proven, quick commercial loan fundings in 2-3 weeks.`,
+  siteUrl: `https://elevatedequities.com/`,
+  themeColor: `#BFD11D`,
+  backgroundColor: `#fff`,
+  pathPrefix: null,
+  logo: path.resolve(__dirname, 'src/images/icon.png'),
+  social: {
+    twitter: ``,
+    fbAppId: ``,
+  },
+};
